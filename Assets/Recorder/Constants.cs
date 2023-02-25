372e36233d6f405d3a387944b4eb14c323f0c3b5
Seyed Moein Saadati <saadati.moin@gmail.com>
2023-02-25 23:23:23 +0330

    Add Constants.cs

---
+++ Assets/Recorder/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Moein.TimeRecorder
+{
+    public static class Constants
+    {
+        public static string ASSETS_PATH = "Assets/Resources/";
+        public static string MAIN_DIRECTORY = "TimeRecordFiles/";
+        public static string RECORD_FILE_EXTENSION = ".objr.bytes";
+    }
+}
