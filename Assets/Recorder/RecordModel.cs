dc8273ee8a7c14a033247432e0e039e072ff9602
Seyed Moein Saadati <saadati.moin@gmail.com>
2023-02-27 22:59:22 +0330

    Add RecordModel.cs

---
+++ Assets/Recorder/RecordModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Moein.TimeRecorder
+{
+    public abstract class RecordModel<T, DataModel>
+    {
+        protected T target;
+
+        public string fileName;
+        public int tapeIndex;
+        List<DataModel> tape = new List<DataModel>();
+
+        public abstract DataModel CaptureData();
+        public void SetTarget(T target) => this.target = target;
+    }
+
+    public class TrnasformRecordModel : RecordModel<Transform, TransformModel>
+    {
+        public override TransformModel CaptureData()
+        {
+            return new TransformModel(target.localPosition, target.localEulerAngles);
+        }
+    }
+
+    public class AudioRecordModel : RecordModel<AudioSource, float>
+    {
+        public override float CaptureData()
+        {
+            return target.pitch;
+        }
+    }
+
+}
