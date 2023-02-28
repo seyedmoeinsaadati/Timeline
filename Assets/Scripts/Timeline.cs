b11a01aa0e1a39ab0307d091642256bbe59ca652
Seyed Moein Saadati <saadati.moin@gmail.com>
2023-02-28 23:19:36 +0330

    Add Timeline.cs

---
+++ Assets/Scripts/Timeline.cs
@@ -0,0 +1,41 @@
+﻿using Moein.Timeline;
+using UnityEngine;
+
+public class Timeline : MonoBehaviour
+{
+    protected TimeState timeState = TimeState.Forward;
+    public StoreType storeType = StoreType.NoMemory;
+    public float timeScale = 1;
+
+    private TimelineComponent<Transform, TransformSnapshot> transformTimeline;
+
+    private void Start()
+    {
+        transformTimeline = new TimelineComponent<Transform, TransformSnapshot>(transform);
+    }
+
+    protected void Rewind()
+    {
+    }
+
+    protected void Record()
+    {
+    }
+
+    public void Pause()
+    {
+    }
+
+    public void StartRewind()
+    {
+    }
+
+    public void StopRewind()
+    {
+    }
+
+    public void ChangeTimeState(TimeState newTimeState)
+    {
+        timeState = newTimeState;
+    }
+}
