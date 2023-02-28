cac4325bef2bf9a5c3935cd7d76a6215692a9b55
Seyed Moein Saadati <saadati.moin@gmail.com>
2023-03-01 00:36:47 +0330

    Modify some files

--- Assets/Scripts/Recorder/RecordModel.cs
+++ Assets/Scripts/Recorder/RecordModel.cs
@@ -20,29 +20,9 @@ namespace Moein.Timeline
 
     public class TransformRecordModel : RecordModel<Transform, TransformSnapshot>
     {
-        // public override void CaptureData()
-        // {
-        //     var model = new TransformSnapshot(component.localPosition, component.localRotation);
-        //     tape.Add(model);
-        // }
-        //
-        // public override void SetData(TransformSnapshot dataModel)
-        // {
-        //     component.localPosition = dataModel.position;
-        //     component.localRotation = dataModel.rotation;
-        // }
     }
 
     public class AudioRecordModel : RecordModel<AudioSource, float>
     {
-        // public override void CaptureData()
-        // {
-        //     tape.Add(component.pitch);
-        // }
-        //
-        // public override void SetData(float dataModel)
-        // {
-        //     component.pitch = dataModel;
-        // }
     }
 }
